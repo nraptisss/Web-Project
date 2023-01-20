85d4e3b09e8cb37e5efe68eb188a6a46c66827ff
Nikos Raptis <104900174+nraptisss@users.noreply.github.com>
2023-01-20 11:47:49 +0200

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const cron = require('node-cron');
 // Use the cors middleware to enable CORS
 app.use(cors());
 
@@ -13,6 +14,16 @@ const jwt = require('./jwt');
 app.use('/', get);
 app.use('/', post);
 app.use('/', jwt);
+
+// Schedule a task to run at the end of every month at 23:59:59
+cron.schedule('59 59 23 L * *', async () => {
+    try {
+        await distributeTokens();
+        console.log('Tokens distributed successfully');
+    } catch (error) {
+        console.log('Error distributing tokens:', error);
+    }
+});
 
 // Start the server on port 3000
 app.listen(3000, () => {
